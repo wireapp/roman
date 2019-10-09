2af1a64ec1e5683c296537a62b2a4cd4177d5f24
dkovacevic <dejankov@gmail.com>
2019-10-09 14:47:53 +0200

    version bump

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 mvn package -DskipTests=true -Dmaven.javadoc.skip=true
-docker build -t $DOCKER_USERNAME/roman:latest .
+docker build -t $DOCKER_USERNAME/roman:1.1.0 .
 docker push $DOCKER_USERNAME/roman
-kubectl delete pod -l name=roman
-kubectl get pods -l name=roman
