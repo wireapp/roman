f0eb13576a5f7128326db078b59ef49633a84986
Lukas Forst <lukas@forst.pw>
2021-05-23 20:31:19 +0200

    add complete guide how to create and use a bot with Roman

--- src/main/resources/db/migration/V102__broadcast.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Broadcast (
- broadcast_id UUID NOT NULL,
- bot_id UUID NOT NULL,
- provider UUID NOT NULL,
- message_id UUID NOT NULL,
- message_status INTEGER NOT NULL,
- created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
- PRIMARY KEY(broadcast_id, message_id, message_status)
-);
