eb975892a4b36442a29839a336d76595996cfc3d
Dejan <dejankov@gmail.com>
2022-10-03 18:58:22 +0200

    added exclusions

--- backend/src/test/java/com/wire/bots/roman/integrations/WebSocketTest.java
+++ backend/src/test/java/com/wire/bots/roman/integrations/WebSocketTest.java
@@ -19,15 +19,33 @@ import java.net.URI;
 import java.util.UUID;
 
 public class WebSocketTest {
-    @ClientEndpoint
-    static class WebsocketClientEndpoint {
-        public WebsocketClientEndpoint(URI endpointURI) {
+    URI wss = null;
+
+    @ClientEndpoint(decoders = WebSocketTest._Decoder.class)
+    public class WebsocketClientEndpoint {
+        Session session;
+
+        public WebsocketClientEndpoint() {
             try {
                 WebSocketContainer container = ContainerProvider.getWebSocketContainer();
-                container.connectToServer(this, endpointURI);
+                session = container.connectToServer(this, wss);
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
+        }
+
+        @OnOpen
+        public void onOpen(Session session) {
+            System.out.printf("Websocket open: %s\n", session.getId());
+        }
+
+        @OnMessage
+        public void onMessage(Object payload) {
+        }
+
+        @OnClose
+        public void onClose(Session closed, CloseReason reason) {
+            System.out.printf("Websocket closed: %s: reason: %s\n", closed.getId(), reason.getCloseCode());
         }
     }
 
@@ -52,7 +70,7 @@ public class WebSocketTest {
     }
 
     @Test
-    public void connectTest() {
+    public void connectTest() throws Exception {
         final UUID botId = UUID.randomUUID();
         final UUID providerId = UUID.randomUUID();
         final String serviceAuth = Tools.generateToken(botId);
@@ -70,12 +88,39 @@ public class WebSocketTest {
         final String wssUrl = "ws://localhost:8080";
         String appKey = provider.serviceAuth;
 
-        URI wss = client.target(wssUrl)
+        wss = client.target(wssUrl)
                 .path("await")
                 .path(appKey)
                 .getUri();
 
-        final WebsocketClientEndpoint clientEndPoint = new WebsocketClientEndpoint(wss);
+        final WebsocketClientEndpoint clientEndPoint = new WebsocketClientEndpoint();
 
+        Thread.sleep(2000);
+
+        clientEndPoint.session.close();
+
+        Thread.sleep(2000);
+    }
+
+    public static class _Decoder implements Decoder.Text<Object> {
+        @Override
+        public Object decode(String s) {
+            return new Object();
+        }
+
+        @Override
+        public boolean willDecode(String s) {
+            return s.startsWith("{") && s.endsWith("}");
+        }
+
+        @Override
+        public void init(EndpointConfig config) {
+
+        }
+
+        @Override
+        public void destroy() {
+
+        }
     }
 }
